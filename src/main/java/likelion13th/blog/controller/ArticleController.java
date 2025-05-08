86777d66198626f8b724ea8b1b6c68676a80d86e
dia3065 <dja306500@gmail.com>
2025-05-08 23:49:21 +0900

    first commit

---
+++ src/main/java/likelion13th/blog/controller/ArticleController.java
@@ -0,0 +1,4 @@
+package likelion13th.blog.controller;
+
+public class ArticleController {
+}
---
+++ src/main/java/likelion13th/blog/domain/Article.java
@@ -0,0 +1,30 @@
+package likelion13th.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    private String password;
+    private LocalDateTime createdAt;
+
+    public Article(String content, Long id, String title, String author, String password) {
+        this.content = content;
+        this.id = id;
+        this.title = title;
+
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public LocalDateTime getCreatedAt(){return createdAt;}
+    public Long getId(){return id;}
+    public String getTitle(){return title;}
+    public String getAuthor(){return author;}
+    public String getPassword(){return password;}
+
+}
