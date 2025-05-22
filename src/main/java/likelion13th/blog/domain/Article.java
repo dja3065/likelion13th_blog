6a6d8816d7de71af983cdbb420c148773192ca4f
dia3065 <dja306500@gmail.com>
2025-05-22 21:08:43 +0900

    Feat:H2 DB연동 및 domain 코드 리팩토링

--- src/main/java/likelion13th/blog/domain/Article.java
+++ src/main/java/likelion13th/blog/domain/Article.java
@@ -1,43 +1,88 @@
 package likelion13th.blog.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+//import java.time.LocalDateTime;
+//
+//public class Article {
+//
+//    private Long id; //고유 번호
+//    private String title; //제목
+//    private String content; //게시글
+//    private String author; //작성자
+//    private String password; //비밀번호
+//    private LocalDateTime createdAt; //생성일
+//
+//    public Article(String content, Long id, String title, String author,String password) {
+//        this.content = content;
+//        this.id = id;
+//        this.title = title;
+//        this.author = author;
+//        this.password = password;
+//        this.createdAt = LocalDateTime.now();
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public LocalDateTime getCreatedAt() {
+//        return createdAt;
+//    }
+//
+//    public Long getId() {return id; }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public String getAuthor() {
+//        return author;
+//    }
+//
+//    public String getPassword() {return password;}
+//}
+//
+@Entity
+@Getter @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Article {
 
-    private Long id; //고유 번호
-    private String title; //제목
-    private String content; //게시글
-    private String author; //작성자
-    private String password; //비밀번호
-    private LocalDateTime createdAt; //생성일
+    @Id // id 필드를 기본키(Primary Key)로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
+    @Column(updatable = false)
+    private Long id;
 
-    public Article(String content, Long id, String title, String author,String password) {
-        this.content = content;
-        this.id = id;
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private String author;
+
+    public Article(String title, String content, String author,  String password) {
         this.title = title;
+        this.content = content;
         this.author = author;
         this.password = password;
         this.createdAt = LocalDateTime.now();
     }
 
-    public String getContent() {
-        return content;
+    public void update(String title, String content){
+        this.title=title;
+        this.content=content;
     }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public Long getId() {return id; }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getPassword() {return password;}
 }
-
--- src/main/java/likelion13th/blog/service/ArticleService.java
+++ src/main/java/likelion13th/blog/service/ArticleService.java
@@ -1,50 +1,106 @@
 package likelion13th.blog.service;
-
-
+//
+//
+import jakarta.persistence.EntityNotFoundException;
 import likelion13th.blog.domain.Article;
-import org.springframework.stereotype.Service;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.NoSuchElementException;
+//
+//@Service
+//public class ArticleService {
+//    private final List<Article> articleDB = new ArrayList<>(); //데이터를 저장할 임시DB
+//    private  Long nextId = 1L; //고유번호 업데이트
+//
+//    public Article addArticle(Article article){
+//        //Article 객체 생성
+//
+//        if(article.getContent()==null
+//        || article.getTitle()==null
+//        || article.getAuthor()==null
+//        || article.getPassword()==null){
+//            throw new IllegalArgumentException("제목, 내용, 작성자, 비밀번호는 필수 입력 사항입니다.");
+//        }
+//        Article newArticle = new Article(
+//                article.getContent(),
+//                nextId++,
+//                article.getTitle(),
+//                article.getAuthor(),
+//                article.getPassword()
+//        );
+//        //DB에 객체 저장
+//        articleDB.add(newArticle);
+//
+//        return newArticle;
+//    }
+//
+//    public List<Article> findAll(){
+//        return articleDB;
+//    }
+//
+//    public Article findById(Long id){
+//        for(Article article: articleDB){
+//            if(article.getId().equals(id)){
+//                return article;
+//            }
+//        }
+//        throw new NoSuchElementException("해당 ID의 게시글을 찾을 수 없습니다.");
+//    }
+//}
 
-import java.util.ArrayList;
+import likelion13th.blog.dto.SimpleArticleResponse;
+import lombok.*;
+import org.springframework.stereotype.Service;
+import likelion13th.blog.repository.ArticleRepository;
+import likelion13th.blog.dto.ArticleResponse;
+import likelion13th.blog.dto.AddArticleRequest;
+
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
+@RequiredArgsConstructor
 public class ArticleService {
-    private final List<Article> articleDB = new ArrayList<>(); //데이터를 저장할 임시DB
-    private  Long nextId = 1L; //고유번호 업데이트
+    private final ArticleRepository articleRepository;
 
-    public Article addArticle(Article article){
-        //Article 객체 생성
+    //게시글 생성
+    public ArticleResponse addArticle(AddArticleRequest request){
+        //1. request 객체의 .toEntity()를 통해 Article 객체 생성
+        Article article=request.toEntity();
 
-        if(article.getContent()==null
-        || article.getTitle()==null
-        || article.getAuthor()==null
-        || article.getPassword()==null){
-            throw new IllegalArgumentException("제목, 내용, 작성자, 비밀번호는 필수 입력 사항입니다.");
-        }
-        Article newArticle = new Article(
-                article.getContent(),
-                nextId++,
-                article.getTitle(),
-                article.getAuthor(),
-                article.getPassword()
-        );
-        //DB에 객체 저장
-        articleDB.add(newArticle);
+        //2. Article객체를 JPA의 save() 를 사용하여 DB에 저장
+        articleRepository.save(article);
 
-        return newArticle;
+       /* 3. article 객체를 response DTO 생성하여 반환
+          response 클래스의 정작 팩토리 메서드 of() 통해 API 응답객체 생성 */
+        return ArticleResponse.of(article);
     }
 
-    public List<Article> findAll(){
-        return articleDB;
+
+    //전체 글 조회
+    public List<SimpleArticleResponse> getAllArticles(){
+        /*1. JPA의 findAll() 을 사용하여 DB에 저장된 전체 Article을 List 형태로 가져오기*/
+        List<Article> articleList = articleRepository.findAll();
+
+        /*2. Article -> SimpleArticleResponse : 엔티티를 DTO로 변환*/
+        List<SimpleArticleResponse> articleResponseList = articleList.stream()
+                .map(article -> SimpleArticleResponse.of(article))
+                .toList();
+
+        /*3. articleResponseList (DTO 리스트) 반환 */
+        return articleResponseList;
     }
 
-    public Article findById(Long id){
-        for(Article article: articleDB){
-            if(article.getId().equals(id)){
-                return article;
-            }
-        }
-        throw new NoSuchElementException("해당 ID의 게시글을 찾을 수 없습니다.");
+    //단일 글 조회
+    public ArticleResponse getArticle(Long id){
+        /* 1. JPA의 findById()를 사용하여 DB에서 id가 일치하는 게시글 찾기.
+              id가 일치하는 게시글이 DB에 없으면 에러 반환*/
+        Article article=articleRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("해당 ID의 게시글을 찾을 수 없습니다. ID: "+id));
+
+        /*2. ArticleResponse DTO 생성하여 반환 */
+        return ArticleResponse.of(article);
     }
+
 }
